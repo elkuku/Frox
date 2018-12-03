1c0e50eed7e5fbfc18efdf42e0121eed34f50e18
Nikolai Plath <der.el.kuku@gmail.com>
2018-12-03 09:02:34 -0500

    Add backup

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,11 +3,11 @@
 namespace App\Controller;
 
 use App\Repository\WaypointRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends AbstractController
+class DefaultController extends Controller
 {
     /**
      * @Route("/", name="default")
@@ -36,5 +36,58 @@ class DefaultController extends AbstractController
     public function provinces(): Response
     {
         return $this->render('default/provinces.html.twig');
+    }
+
+    /**
+     * @Route("/backup", name="backup")
+     *
+     * @return Response
+     */
+    public function backup(): Response
+    {
+        $pattern = '#mysql://(.+)\:(.+)@127.0.0.1:3306/(.+)#';
+
+        preg_match($pattern, getenv('DATABASE_URL'), $matches);
+
+        if (4 !== \count($matches))
+        {
+            throw new \UnexpectedValueException('Error parsing the database URL.');
+        }
+
+        $dbUser = $matches[1];
+        $dbPass = $matches[2];
+        $dbName = $matches[3];
+
+        $cmd = sprintf('mysqldump -u%s -p%s %s|gzip 2>&1', $dbUser, $dbPass, $dbName);
+
+        ob_start();
+        passthru($cmd, $retVal);
+        $gzip = ob_get_clean();
+
+        if ($retVal)
+        {
+            throw new \RuntimeException('Error creating DB backup: ' . $gzip);
+        }
+
+        $fileName = date('Y-m-d') . '_backup.gz';
+        $mime     = 'application/x-gzip';
+
+        $message = (new \Swift_Message('Frox! Backup', '<h3>Backup</h3>Date: ' . date('Y-m-d'), 'text/html'))
+            ->attach(new \Swift_Attachment($gzip, $fileName, $mime))
+            ->setFrom(getenv('MAILER_FROM_MAIL'))
+            ->setTo(getenv('MAILER_FROM_MAIL'));
+
+        $count = $this->get('mailer')->send($message);
+
+        if (!$count)
+        {
+            $this->addFlash('danger', 'There was an error sending the message...');
+        }
+        else
+        {
+            $this->addFlash('success', 'Backup has been sent to your inbox.');
+        }
+
+        return $this->redirectToRoute('default');
     }
 }
