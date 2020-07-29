4e4893b131afe901bd06379d1a66bd10071c3a48
elkuku <der.el.kuku@gmail.com>
2020-07-29 10:58:34 -0500

    [DBMIGRATION]]

---
+++ src/Migrations/Version20200729155006.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200729155006 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE agent_chat DROP FOREIGN KEY FK_C80648493414710B');
+        $this->addSql('ALTER TABLE agent_chat DROP FOREIGN KEY FK_C80648491A9A7125');
+        $this->addSql('DROP TABLE agent');
+        $this->addSql('DROP TABLE agent_chat');
+        $this->addSql('DROP TABLE chat');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE agent (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, lat NUMERIC(10, 6) NOT NULL, lon NUMERIC(10, 6) NOT NULL, city VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('CREATE TABLE agent_chat (agent_id INT NOT NULL, chat_id INT NOT NULL, INDEX IDX_C80648493414710B (agent_id), INDEX IDX_C80648491A9A7125 (chat_id), PRIMARY KEY(agent_id, chat_id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('CREATE TABLE chat (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE agent_chat ADD CONSTRAINT FK_C80648491A9A7125 FOREIGN KEY (chat_id) REFERENCES chat (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE agent_chat ADD CONSTRAINT FK_C80648493414710B FOREIGN KEY (agent_id) REFERENCES agent (id) ON DELETE CASCADE');
+    }
+}
