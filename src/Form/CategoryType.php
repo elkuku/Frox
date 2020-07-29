aab1171a1b9843338a35fefc7b47369ffa45793b
elkuku <der.el.kuku@gmail.com>
2020-07-29 10:55:57 -0500

    Add category

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -2,25 +2,24 @@
 
 namespace App\Form;
 
-use App\Entity\Chat;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ChatType extends AbstractType
+class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name');
+            ->add('name')
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults(
-            [
-                'data_class' => Chat::class,
-            ]
-        );
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+        ]);
     }
 }
