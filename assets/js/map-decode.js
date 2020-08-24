eb600118e90f4dca41a346ed2ca70e76b9b25854
Nikolai Plath <der.el.kuku@gmail.com>
2020-08-24 16:41:26 -0500

    Update d3c0de feature

--- assets/js/map-decode.js
+++ assets/js/map-decode.js
@@ -1,17 +1,6 @@
 require('leaflet')
 require('leaflet/dist/leaflet.css')
 
-// require('leaflet.markercluster')
-// require('leaflet.markercluster/dist/MarkerCluster.css')
-// require('leaflet.markercluster/dist/MarkerCluster.Default.css')
-
-require('leaflet-draw')
-require('leaflet-draw/dist/leaflet.draw.css')
-// const $ = require('jquery');
-// global.$ = global.jQuery = $;
-// require('jquery-sortable-lists')
-// require('html5sortable/docs/html5sortable')
-
 require('jquery-ui-dist/jquery-ui')
 require('jquery-ui-dist/jquery-ui.css')
 
@@ -21,7 +10,7 @@ $('#sortable').sortable({
     axis: 'y',
     cursor: 'move',
     containment: 'parent',
-    update: function (event, ui) {
+    update: function () {
         const ids = $(this).sortable('toArray')
         updatePolyLine(ids)
         updateIntelLink(ids)
@@ -32,88 +21,24 @@ const map = new L.Map('map')
 
 let wayPoints = window.wayPoints, polyLine
 
-// const markers = L.markerClusterGroup({disableClusteringAtZoom: 16})
-
 function initmap() {
     const osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
     const osmAttrib = 'Map data (C) <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
     const osm = new L.TileLayer(osmUrl, {attribution: osmAttrib})
+    map.addLayer(osm)
 
     const firstPoint = window.wayPoints[0]
     if (firstPoint) {
         map.setView(new L.LatLng(firstPoint.lat, firstPoint.lng), 15)
     } else {
         map.setView(new L.LatLng(0.990275, -79.659482), 9)
-
     }
-    // let lng = new L.LatLng(firstPoint.lat, firstPoint.lng)
-
-    map.addLayer(osm)
-
-    const editableLayers = new L.FeatureGroup()
-    map.addLayer(editableLayers)
-
-    const drawPluginOptions = {
-        position: 'topright',
-        draw: {
-            polygon: {
-                allowIntersection: false, // Restricts shapes to simple polygons
-                drawError: {
-                    color: '#e1e100', // Color the shape will turn when intersects
-                    message: '<strong>Oh snap!<strong> you can\'t draw that!' // Message that will show when intersect
-                },
-                shapeOptions: {
-                    color: '#97009c'
-                }
-            },
-            // disable toolbar item by setting it to false
-            // polyline: true,
-            circle: false,
-            rectangle: true,
-            marker: false,
-        },
-        edit: {
-            featureGroup: editableLayers, //REQUIRED!!
-            remove: false
-        }
-    }
-
-    const drawControl = new L.Control.Draw(drawPluginOptions)
-    map.addControl(drawControl)
-
-    map.on('draw:created', function (e) {
-        const type = e.layerType,
-            layer = e.layer
-
-        if (type === 'marker') {
-            layer.bindPopup('A popup!')
-        }
-
-        editableLayers.addLayer(layer)
-    })
 }
 
 function loadMarkers() {
-
-    // markers.clearLayers()
-
-    // const myIcon = L.icon({
-    //     iconUrl: '/build/img/ico/my-icon.png',
-    //     iconSize: [22, 36],
-    //     iconAnchor: [11, 36],
-    //     popupAnchor: [0, -18],
-    // })
-
-    // console.log(window.wayPoints)
-
-    // $.get('/waypoints_map', function (data) {
-    //     $(data)
     let pointList = []
     $(window.wayPoints).each(function () {
         const pic = $('#' + this.id).find('img').attr('src')
-        // const pic = sElement.find('img').attr('src')
-        // console.log(pic)
-        // console.log(this.guid)
         let lng = new L.LatLng(this.lat, this.lng)
         pointList.push(lng)
         let wpIcon = L.icon({
@@ -140,9 +65,6 @@ function loadMarkers() {
                 popup.update()
             })
         })
-
-        // markers.addLayer(marker)
-        // map.addLayer(markers)
     })
 
     polyLine = new L.Polyline(pointList, {
@@ -204,8 +126,13 @@ function updateIntelLink(ids) {
     let text = 'http://intel.ingress.com/intel?ll=' + center + '&z=15&pls=' + links.join('_')
 
     $('#intelLink').val(text)
+    $('#intelLinkHref').attr('href', text)
     $('#intelLinkList').val(linkList.join("\n"))
 }
 
 initmap()
 loadMarkers()
+const ids = $('#sortable').sortable('toArray')
+updatePolyLine(ids)
+updateIntelLink(ids)
+
