999f5be97ad2ef895dc243a1d08f7cbdabf56134
Nikolai Plath <der.el.kuku@gmail.com>
2021-08-04 12:29:53 +0200

    Update deps

--- assets/app.js
+++ assets/app.js
@@ -5,20 +5,15 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
-//import './styles/app.css';
-
 // start the Stimulus application
 import './bootstrap';
 
 const $ = require('jquery')
 
 require('bootstrap')
-// require('bootstrap/dist/css/bootstrap.css')
 require('bootswatch/dist/slate/bootstrap.css')
 require('open-iconic/font/css/open-iconic-bootstrap.css')
 
-// any CSS you require will output into a single css file (app.css in this case)
 require('./css/app.css')
 
 $(function () {
